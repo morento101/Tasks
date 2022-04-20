cce57f3fd0fc4d1c79eb264725ec4445d9a90d84
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-04-20 13:41:53 +0300

    All tasks are complited

---
+++ 19.04.22/554.py
@@ -0,0 +1,28 @@
+from typing import List
+from time import time
+
+
+def pythagorean_triples(n: int) -> List[tuple]:
+    """
+    Returns list of tuples with all pythagorean triples triples which satisfy
+    following equations: a^2 + b^2 = c^2 and a <= b <= c <= n
+    """
+
+    res = []
+    domain = range(1, n+1)
+    n_square = n ** 2
+
+    for a in domain:
+        for b in domain:
+            c = (a ** 2 + b ** 2)
+            if c <= n_square:
+                if not c ** .5 % 1:
+                    res.append((a, b, int(c**.5)))
+
+    return res
+
+
+if __name__ == '__main__':
+    start = time()
+    print(pythagorean_triples(50))
+    print(time() - start)
